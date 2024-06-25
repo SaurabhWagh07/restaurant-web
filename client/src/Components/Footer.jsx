06a41365a879d0157cb4ee053a6f85a91a2a099f
SaurabhWagh07 <saurabhwagh0082@gmail.com>
2024-06-25 16:15:09 +0530

    first commit

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Tiger SW</div>
+          <div className="right">
+            <p>Sinhgad Road, Pune, Maharashtra, 411041</p>
+            <p>Open: 07:00 AM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By SAURABH WAGH</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By @Saurabh Wagh</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
